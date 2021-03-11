319a3fb8f1c4a42a1ac40aa02b7e37cafaa0e6f0
Aman Nanda <34944818+amannanda8@users.noreply.github.com>
2021-03-11 16:46:40 +0530

    revision

---
+++ lib/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quiz_app/authentication_service.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Signin Page'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              'Home Page',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {
+              context.read<AuthenticationService>().signOut();
+            },
+            child: Text('Sign Out'),
+          ),
+        ],
+      ),
+    );
+  }
+}
